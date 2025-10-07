1bb5764775f8fde0c122c22ca4fbd245af1c26b5
omriomri31 <omriomrinir@gmail.com>
2025-10-07 22:38:11 +0300

    build: add JUnit5, Appium client, logging; enable JUnit Platform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,29 @@
-plugins {
-    id("java")
-}
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
+plugins { java }
 
 repositories {
     mavenCentral()
+    google()
+}
+
+java {
+    toolchain { languageVersion.set(JavaLanguageVersion.of(11)) }
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+
+    implementation("io.appium:java-client:9.2.1")
+    implementation("org.seleniumhq.selenium:selenium-support:4.21.0")
+
+    implementation("org.slf4j:slf4j-api:2.0.13")
+    runtimeOnly("ch.qos.logback:logback-classic:1.4.14")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events("PASSED","FAILED","SKIPPED")
+    }
 }
